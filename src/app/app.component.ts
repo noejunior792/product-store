3f868897dcd42ce731c59a18291f822e9ab4f23f
Noé Júnior <noedombaxe@gmail.com>
2024-06-15 21:18:09 +0100

    feat: add json-server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { ListComponent } from './features/list/list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent,ListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'product-store';
-}
+export class AppComponent {}
