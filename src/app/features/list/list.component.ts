f8a68d336694d1252d3f876c33becbef55719967
Noé Júnior <noedombaxe@gmail.com>
2024-06-20 23:18:29 +0100

    feat: add card

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,17 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { Component, importProvidersFrom, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/product.interface';
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
 
-  products: any[] = [];
+  products: Product[] = [];
 
   productsServices = inject(ProductsService);
 
