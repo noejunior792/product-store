ec61fea3b29148e627a0e0bc642e032cdc86ded9
Noé Júnior <noedombaxe@gmail.com>
2024-06-21 20:44:53 +0100

    refactor: refactor list of products creating a component for render

---
+++ src/app/features/list/components/card/card.component.ts
@@ -0,0 +1,20 @@
+import { Component, computed, input } from '@angular/core';
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { Product } from '../../../../shared/interfaces/product.interface';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatCardModule,MatButtonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+
+
+export class CardComponent {
+
+  product = input.required<Product>();
+
+  productTitle = computed(() => this.product().title)
+}
