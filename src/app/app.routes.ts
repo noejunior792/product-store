ef756f1abb5b10a2ce46f9cfd633ae600b19b948
Noé Júnior <noedombaxe@gmail.com>
2024-06-22 11:43:07 +0100

    refactor: charge CreateComponent with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,9 @@ export const routes: Routes = [{
     component: ListComponent
 }, {
     path: 'create-product',
-    component: CreateComponent
-}];
+    loadComponent: () =>
+        import('./features/create/create.component').then(
+            (m) => m.CreateComponent
+        ),
+},
+];
