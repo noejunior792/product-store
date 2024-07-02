98d7d40a800ea23dc0436660c89e73eede45b635
Noé Júnior <noedombaxe@gmail.com>
2024-07-02 10:17:38 +0100

    refactor: create a form component for reutiliazing

--- src/app/features/edit/edit.component.ts
+++ src/app/features/edit/edit.component.ts
@@ -1,17 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { ProductsService } from '../../shared/services/products.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../../shared/interfaces/product.interface';
+import { FormComponent } from '../../shared/components/form/form.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormComponent],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
@@ -22,20 +19,11 @@ export class EditComponent {
   router = inject(Router);
   product: Product = inject(ActivatedRoute).snapshot.data['product'];
 
-  form = new FormGroup({
-    title: new FormControl<string>(this.product.title, {
-      nonNullable: true, validators: Validators.required
+  onSubmit(product: Product) {
 
-    }),
-  })
-
-  onSubmit() {
-
-    this.productService.put(this.product.id, {
-      title: this.form.controls.title.value
-    })
-      .subscribe(() => {
+    this.productService.put(this.product.id, product).subscribe(() => {
         this.matSnackBar.open('Produto eidtado com sucesso!', 'Ok');
+
         this.router.navigateByUrl('/').catch(console.log)
       });
   }
---
+++ src/app/shared/components/form/form.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, input, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Product } from '../../interfaces/product.interface';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent {
+
+  product = input<Product | null>(null);
+  form!: FormGroup;
+
+  @Output() done = new EventEmitter<Product>();
+
+  ngOnInit(): void{
+    this.form = new FormGroup({
+      title: new FormControl<string>(this.product()?.title || '' ,{
+        nonNullable: true,
+        validators: Validators.required
+      }),
+    });
+  }
+
+  onSubmit(){
+    const product = this.form.value as Product;
+    this.done.emit(product);
+  }
+}
