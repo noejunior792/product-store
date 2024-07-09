427821f0ed586d49149eca1176a589e826570bd7
Noé Júnior <noedombaxe@gmail.com>
2024-07-09 10:47:57 +0100

    🔥feat: add back-to-list component in component create and edit

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -4,11 +4,12 @@ import { ProductsService } from '../../shared/services/products.service';
 import { Router } from '@angular/router';
 import { FormComponent } from '../../shared/components/form/form.component';
 import { Product } from '../../shared/interfaces/product.interface';
+import { BackToListComponent } from '../../shared/components/back-to-list/back-to-list.component';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, BackToListComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
